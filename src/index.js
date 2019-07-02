34b640a79dc20a896734993ab5f07c578b956711
qwas3110 <283994917@qq.com>
2019-07-02 11:52:46 +0800

    使用Redux 开发工具

--- src/index.js
+++ src/index.js
@@ -7,12 +7,15 @@ import { createStore } from 'redux';
 import reducer from './reducers';
 
 
-const store = createStore(reducer);
+
+
+
+const store = createStore(
+    reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 console.log(store);
-
-console.log(store.getState());
-
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
