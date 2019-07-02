19ffaed49804d8443f9143d0ba8f9e1068df6a30
qwas3110 <283994917@qq.com>
2019-07-02 14:25:45 +0800

    首次引入 store

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 import { createStore } from 'redux';
 import reducer from './reducers';
@@ -15,9 +15,12 @@ const store = createStore(
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
-console.log(store);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// 将store 传递给应用组件，现在应用将接收 store 作为 props
+ReactDOM.render(
+    <App store={store} />,
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
