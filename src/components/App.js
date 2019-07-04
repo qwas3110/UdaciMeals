7627ce871cea2f77b9f33dd8dc296a59f74903fb
qwas3110 <283994917@qq.com>
2019-07-04 10:46:09 +0800

    connect to mapStateToProps

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 
 
 class App extends React.Component{
 
+
     render() {
+
+        console.log('Props:',this.props);
+
         return (
             <div>
                 Hello World!
@@ -14,4 +19,42 @@ class App extends React.Component{
 }
 
 
-export default App;
+// 此函数将我们Redux 状态映射到组件props
+
+function mapStateToProps(calendar) {
+    // 创建星期数组
+    const dayOrder = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
+    // 返回一个具有calendar属性的对象
+    return {
+        calendar: dayOrder.map((day) => ({
+            day,
+            meals: Object.keys(calendar[day]).reduce((meals,meal) => {
+                meals[meal] = calendar[day][meal]
+                    ? calendar[day][meal]
+                    : null;
+                return meals;
+            },{})
+        }))
+    }
+}
+
+
+
+export default connect(mapStateToProps)(App);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
