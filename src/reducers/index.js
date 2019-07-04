9d12bffa3917a34bc0f705ad7e92167dfa3284f7
qwas3110 <283994917@qq.com>
2019-07-04 14:06:48 +0800

    food reducer init

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,6 +3,28 @@ import {
     REMOVE_FROM_CALENDAR,
 } from '../actions';
 
+// 创建food reducer
+
+function food(state = {}, action) {
+
+    switch (action.type) {
+        case ADD_RECIPE:
+            const { recipe }  = action;
+
+            return {
+                // 无论当前状态是什么，我们都将它赋值给state
+                ...state,
+                [recipe.label]: recipe
+            };
+        default:
+            return state
+    }
+
+}
+
+
+
+
 const initialCalendarState = {
     sunday: {
         breakfast: null,
