8bba01effe13daa9ea32c955dd95b19c563ab0be
qwas3110 <283994917@qq.com>
2019-07-06 09:21:24 +0800

    Calendar 基本布局

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,55 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { addRecipe,removeFromCalendar } from '../actions';
+import { capitalize } from '../utils/helpers'
+import CalendarIcon from 'react-icons/lib/fa/calendar-plus-o'
 
 
-
-class App extends React.Component{
-
-    doThing = () => {
-        this.props.selectRecipe({})
-    };
-
-
+class App extends React.Component {
     render() {
+        const { calendar, remove } = this.props
+        const mealOrder = ['breakfast', 'lunch', 'dinner'];
 
-        console.log('Props:',this.props);
 
         return (
-            <div>
-                Hello World!
+            <div className='container'>
+
+                <ul className='meal-types'>
+                    {mealOrder.map((mealType) => (
+                        <li key={mealType} className='subheader'>
+                            {capitalize(mealType)}
+                        </li>
+                    ))}
+                </ul>
+
+                <div className='calendar'>
+                    <div className='days'>
+                        {calendar.map(({ day }) => <h3 key={day} className='subheader'>{capitalize(day)}</h3>)}
+                    </div>
+                    <div className='icon-grid'>
+                        {calendar.map(({ day, meals }) => (
+                            <ul key={day}>
+                                {mealOrder.map((meal) => (
+                                    <li key={meal} className='meal'>
+                                        {meals[meal]
+                                            ? <div className='food-item'>
+                                                <img src={meals[meal].image} alt={meals[meal].label}/>
+                                                <button onClick={() => remove({meal, day})}>Clear</button>
+                                            </div>
+                                            : <button className='icon-btn'>
+                                                <CalendarIcon size={30}/>
+                                            </button>}
+                                    </li>
+                                ))}
+                            </ul>
+                        ))}
+                    </div>
+                </div>
+
             </div>
-        );
+        )
     }
 }
-
 
 // 此函数将我们Redux 状态映射到组件props
 
