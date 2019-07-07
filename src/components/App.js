d6deb860f1aadab98c32b1808163fcc7ec4c5801
qwas3110 <283994917@qq.com>
2019-07-07 19:37:53 +0800

    构建Food Modal

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,87 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { addRecipe,removeFromCalendar } from '../actions';
-import { capitalize } from '../utils/helpers'
-import CalendarIcon from 'react-icons/lib/fa/calendar-plus-o'
+import { capitalize } from '../utils/helpers';
+import CalendarIcon from 'react-icons/lib/fa/calendar-plus-o';
+// 导入模态框
+import Modal from 'react-modal';
+// 导入right-icon 图标
+import ArrowRightIcon from 'react-icons/lib/fa/arrow-circle-right';
+// 导入 Loading 组件
+import Loading from 'react-loading';
+// API 调用文件
+import { fetchRecipes } from '../utils/api';
+
+import FoodList from './FoodList'
+
+//openFoodModal 打开模态框
+// 将在本地组件上设置状态，foodModalOpen变为true，然后传入meal和day
+
+//closeFoodModal 关闭模态框
+
+
+//searchFood 调用api，搜索食谱等
+// 将loadingFood 设为true,
+// fetchRecipes，向它传递输入值,我们将获得一个食物数组,
+// 解析后，我们设置食物的本地状态,将此状态保存到本地组件状态
+
 
 
 class App extends React.Component {
+
+    state = {
+        foodModalOpen: false,
+        meal:null,
+        day: null,
+        food:null,
+        loadingFood: false,
+    };
+
+    openFoodModal = ({ meal, day }) => {
+        this.setState(() => ({
+            foodModalOpen: true,
+            meal,
+            day
+        }))
+    };
+
+    closeFoodModal = () => {
+      this.setState(() => ({
+          foodModalOpen: false,
+          meal: null,
+          day: null,
+          food: null
+      }))
+    };
+
+
+    searchFood = (e) => {
+        if (!this.input.value) {
+            return;
+        }
+
+        e.preventDefault();
+
+        this.setState(() => ({ loadingFood: true }))
+
+        fetchRecipes(this.input.value)
+            .then((food) => this.setState(() => ({
+                food,
+                loadingFood: false,
+            })))
+
+
+    };
+
+
+
+
+
     render() {
-        const { calendar, remove } = this.props
+
+
+        const { foodModalOpen, loadingFood, food  } = this.state;
+        const { calendar, remove, selectRecipe } = this.props;
         const mealOrder = ['breakfast', 'lunch', 'dinner'];
 
 
@@ -36,7 +110,10 @@ class App extends React.Component {
                                                 <img src={meals[meal].image} alt={meals[meal].label}/>
                                                 <button onClick={() => remove({meal, day})}>Clear</button>
                                             </div>
-                                            : <button className='icon-btn'>
+                                            // 当被点击时打开模态框,传入meal day ，都来自我们的映射
+                                            : <button className='icon-btn'
+                                                      onClick={() => this.openFoodModal({meal,day})}
+                                                    >
                                                 <CalendarIcon size={30}/>
                                             </button>}
                                     </li>
@@ -45,6 +122,53 @@ class App extends React.Component {
                         ))}
                     </div>
                 </div>
+                {/*渲染一个模态框,当foodModalOpen为true时，则打开此模态框*/}
+                {/*当我们尝试关闭它时 onRequestClose={this.closeFoodModal} 将运行，将改为false，关闭模态框*/}
+                {/*在这里做的是渲染一些div*/}
+                <Modal
+                    className='modal'
+                    overlayClassName='overlay'
+                    isOpen={foodModalOpen}
+                    onRequestClose={this.closeFoodModal}
+                    contentLabel='Modal'
+                >
+                    <div>
+                        {loadingFood === true
+                            ? <Loading delay={200} type='spin' color='#222' className='loading' />
+                            : <div className='search-container'>
+                                <h3 className='subheader'>
+                                    Find a meal for {capitalize(this.state.day)} {this.state.meal}.
+                                </h3>
+                                <div className='search'>
+                                    <input
+                                        className='food-input'
+                                        type='text'
+                                        placeholder='Search Foods'
+                                        ref={(input) => this.input = input}
+                                    />
+                                    {/*每当按钮被点击时.渲染右箭头图标。运行searchFood*/}
+                                    {/*调取fetchRecipes获得食物后。加载到本地状态中*/}
+                                    <button
+                                        className='icon-btn'
+                                        onClick={this.searchFood}>
+                                        <ArrowRightIcon size={30}/>
+                                    </button>
+                                </div>
+                                {/*food不为null，我们渲染食物清单*/}
+                                {/*并向它传递当选中特定项时要做什么， selectRecipe*/}
+                                {/*当selectRecipe运行后将调用this.closeFoodModal()，关闭模态框*/}
+                                {food !== null && (
+                                    <FoodList
+                                        food={food}
+                                        onSelect={(recipe) => {
+                                            selectRecipe({ recipe, day: this.state.day, meal: this.state.meal })
+                                            this.closeFoodModal()
+                                        }}
+                                    />)}
+                            </div>}
+                    </div>
+                </Modal>
+
 
             </div>
         )
