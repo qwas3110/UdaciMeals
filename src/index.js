8d749d9e679b8edcadcdd1e5e26b5cca06e53116
qwas3110 <283994917@qq.com>
2019-07-08 10:58:58 +0800

    构建Redux 中间件logger

--- src/index.js
+++ src/index.js
@@ -3,17 +3,34 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import reducer from './reducers';
 import { Provider } from 'react-redux';
 
 
 
+const logger = store => next => action => {
+    console.log(action.type);
+    //向web控制台输出一个通知信息。在Firefox和Chrome中，web控制台的日志中的项目旁边会显示一个小的‘I‘图标
+    console.info('dispatching',action);
+    //next() 方法返回一个包含属性 done 和 value 的对象。该方法也可以通过接受一个参数用以向生成器传值。
+    let result = next(action);
+    console.log('next state', store.getState());
+    //使用 console.groupEnd() 方法来结束一个分组信
+    console.groupEnd(action.type);
+    return result;
+};
 
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+//window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 
 const store = createStore(
     reducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    composeEnhancers(
+        applyMiddleware(logger)
+    )
 );
 
 
